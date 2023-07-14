dc8473f11138d5e6f10c7d79be1cd71d7e0b8269
eveminyoso <eveminyoso@gmail.com>
2023-07-13 17:20:16 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdion.h)
+/**
+ * main - use of puts function to print a statement
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\""Programming is like building a multilingual puzzle");
+	return (0);
+}
