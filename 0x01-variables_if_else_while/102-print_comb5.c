dc2f7e5af42215b7ba53984f3112ea526987858b
eveminyoso <eveminyoso@gmail.com>
2023-07-17 14:46:44 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,47 +6,31 @@
  *
  */
 int main(void)
-{       
-        int num1, num2, num3, num4
-                
-        num1 = (0);
-        num2 = (0);
-        num3 = (0);
-        num4 = (0);
-        
-        while (num1 <= '9')
-        {       
-                while (num2 <= '9')
-                {&&     
-                        while (num3 <= '9')
-                        {       
-                                while (num4 <= '9')
-                                {       
-                                       if (((num3 + num4) > (num1 + num2) && num3 >= num1) || num1 < num3)
-					{
-						putchar(num1);
-						putchar(num2);
-						putchar(' ');
-						putchar(num3);
-						putchar(num4);
-				 		
-						if (num1 + num2 + num3 + num4 == '179' && num1 == '9')
-						{
-							break;
-						}
-						else
-						{
-						putchar(',');
-						putchar(' ');
-						}
-					}
-		       		}
-	 			num3++;
+{
+	int i, j;
+
+	for (i = 0; i <= 98; i++)
+	{
+		int fdt = i / 10;
+		int sdt = i % 10;
+		for (j = i + 1; j <= 99; j++)
+		{
+			int fdt2 = j / 10;
+			int sdt2 = j % 10;
+
+			putchar('0' + fdt);
+			putchar('0' + sdt);
+			putchar(' ');
+			putchar('0' + fdt2);
+			putchar('0' + sdt2);
+
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
 			}
-			num2++;
 		}
-		num1++;
 	}
-	putchar('\n')
+	putchar('\n');
 	return (0);
-}	
+}
