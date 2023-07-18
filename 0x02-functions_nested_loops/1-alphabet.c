dd83c89b88ed71b41dceadce176b169a5593e9e0
eveminyoso <eveminyoso@gmail.com>
2023-07-18 14:28:21 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - function
+ * Return: Always 0
+ */
+int main(void)
+{
+	void print_alphabet(void)
+	{
+		int num = 97;
+		while (num < 123)
+		{
+			putchar("%c",num)
+		}
+		putchar('\n')
+	}
+
+	return (0);
+}
