09ca8dc82791b7d0c232c571c7b3a76af1c92f5b
eveminyoso <eveminyoso@gmail.com>
2023-07-19 08:08:44 -0700

    5-sign.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,10 @@
 #include "main.h"
 
 /**
- *
+ * print_last_digit - print the last digit of a number
+ * @n: the int to extract the last digit from
+ * Return: value of the lat digit
+ */
+int print_last_digit(int n)
+{
+	int a;
