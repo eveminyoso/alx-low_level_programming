301c4ba5a112a0cfaa4e9c22ea5d7e03dfdc2c62
eveminyoso <eveminyoso@gmail.com>
2023-07-19 08:58:47 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,14 +4,14 @@
  * jack_bauer - prints every minute of the day
  * stariting from 00:00 to 23:59
  */
-void jack_bauer(void)
+void jack_baver(void)
 {
 	int i, j;
 
-	i = 0
+	i = 0;
 	while (i < 24)
 	{
-		j = 0
+		j = 0;
 		while (j < 60)
 		{
 			_putchar((i / 10) + '0');
