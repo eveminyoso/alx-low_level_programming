53080b170b2550635e8d127b6f24acf7b883f326
eveminyoso <eveminyoso@gmail.com>
2023-07-19 09:35:40 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _jack_bauer - prints every minute of the day
+ * jack_baver - prints every minute of the day
  * stariting from 00:00 to 23:59
  */
 void jack_baver(void)
