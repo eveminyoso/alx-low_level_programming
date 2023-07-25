4291d18c59b8a7e86812056bdc896e82f2cc55f4
eveminyoso <eveminyoso@gmail.com>
2023-07-24 18:51:18 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_PASSWORD_LENGTH
+
+/*Function to generate a random password*/
+
+void generatePassword(char *pswd, int len)
+{
+	int i;
+
+	static const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	for (i = 0; i < len; i++)
+	{
+		int index = rand() % (sizeof(charset) - 1);
+		pswd[i] = charset[index];
+	}
+	pswd[len] = '\0';
+}
+int main()
+{
+	int passwordLength;
+	char pswd[MAX_PASSWORD_LENGTH + 1];
+
+	srand((unsigned int)time(NULL));
+	passwordLength = 10;
+
+	generatePassword(pswd, passwordLength);
+	printf("Tada! Congrats\n");
+	return (0);
+}
