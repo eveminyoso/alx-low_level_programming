74f7647153c2dc4abfd064bbeacd128a1c2d2a88
eveminyoso <eveminyoso@gmail.com>
2023-07-27 13:30:37 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,6 @@ char *cap_string(char *str)
 	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
-		{
 			i++;
 
 		if (str[i - 1] == ' ' ||
@@ -33,7 +32,6 @@ char *cap_string(char *str)
 			str[i] -= 32;
 
 		i++;
-		}
 	}
 
 	return (str);
