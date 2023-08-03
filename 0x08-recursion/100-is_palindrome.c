ca63d02d441d89411d51967998950495c289ca09
eveminyoso <eveminyoso@gmail.com>
2023-08-03 12:01:03 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,31 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+int is_palindrome_helper(char *s, int l, int r);
+/**
+ * is_palindrome_helper - checks if a string is empty
+ * @s:the string
+ * @l:number of letters
+ * @r:number of letters
+ *
+ * Return:1 if string is palidrome
+ */
+int is_palindrome_helper(char *s, int l, int r)
+{
+	if (l >= r)
+	{
+		return (1); /* Base case: the string is a palindrome*/
+	}
+
+	if (s[l] != s[r])
+	{
+		return (0); /* Base case: the characters don't match, not a palindrome*/
+	}
+
+	return (is_palindrome_helper(s, l + 1, r - 1)); /*next pair characters*/
+}
 /**
  * is_palindrome - checks if a string is empty
  * @s:the string
@@ -8,20 +34,11 @@
  */
 int is_palindrome(char *s)
 {
-	int l = 0;
-	int r = 0;
+	int length = strlen(s);
 
-	if (NULL == s || 0 > l || 0 > r)
+	if (s == NULL)
 	{
-		return (0);
+		return (0); /* Handle NULL input*/
 	}
-	if (l >= r)
-	{
-		return (1);
-	}
-	if (s[l] == s[r])
-	{
-		return (0);
-	}
-	return (0);
+	return (is_palindrome_helper(s, 0, length - 1));
 }
