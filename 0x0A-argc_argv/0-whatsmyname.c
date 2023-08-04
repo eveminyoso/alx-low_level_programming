c5cfa2b14df26be91d341bf59538ff4ec6041df7
eveminyoso <eveminyoso@gmail.com>
2023-08-04 16:25:32 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - returns the first arguement
+ * @argc: Counts the arguements
+ * @argv: arguements array
+ *
+ * Return:Success value 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
