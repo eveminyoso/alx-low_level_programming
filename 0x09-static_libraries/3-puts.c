743e3a55d0c402ccc51a00ae7e2f959c86a50069
eveminyoso <eveminyoso@gmail.com>
2023-08-05 13:58:53 -0700

    3-puts.o

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,15 +1,15 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * _puts - print a string followed by new line
- * @str: string to print
  * @s: character to be described
  */
 void _puts(char *s)
 {
-	const char* str =  Hello world
-	while (*str != '\0')
+	while (*s != '\0')
 	{
-		putchar(*str++);
+		s++;
 	}
-		_putchar('\n');
+		putchar('\n');
 }
