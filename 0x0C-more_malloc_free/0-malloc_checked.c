ad991a5cc030f9ce22bee45976831f7b8d260aa1
eveminyoso <eveminyoso@gmail.com>
2023-08-10 13:17:28 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory
+ * @b: size of memory
+ *
+ * Return: func
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	s = malloc(b);
+	if (s == NULL)
+	{
+		return (void *)(98);
+	}
+	return (s);
+}
