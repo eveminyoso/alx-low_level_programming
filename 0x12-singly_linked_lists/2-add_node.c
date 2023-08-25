9ee3a1bda2a0e3897a67e1a2771753447320ba57
eveminyoso <eveminyoso@gmail.com>
2023-08-25 14:08:17 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,25 +1,33 @@
 #include "lists.h"
 /**
- * add_node - adds a new node at the beginning of a list_t list
- * @head:stsrting point
- * @str:element
- * Return:list or 0
+ * add_node - adds a new node at the beginning of our
+ * @head: pointer to the head of our linked lists
+ * @str:string to be added and duplicated to the new node
+ * Return: The address of our new element or NULL if it failed
+ *
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *temp;
-	int length = 0;
+	list_t *new_node;
 
-	temp = malloc(sizeof(list_t));
-	if (temp == NULL)
+	if (str == NULL)
+	{
 		return (NULL);
-	while (str[length])
-		length++;
+	}
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);/*duplicating our input string*/
+	if (new_node->str == NULL)
+	{
+		free(new_node);/*Freeing new_node if str is Null*/
+		return (NULL);
+	}
+	new_node->len = strlen(str);
+	new_node->next = *head;/*Setting the pointer of the new node to current head*/
+	*head = new_node;/*updating head to point to the new_node*/
 
-	temp->len = length;
-	temp->str = strdup(str);
-	temp->next = *head;
-	*head = temp;
-	return (temp);
+	return (new_node);
 }
