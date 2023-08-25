2beaa61930c2998fd6d3f4d5a1e013c872065062
eveminyoso <eveminyoso@gmail.com>
2023-08-24 18:38:53 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	size_t node = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
+
+int main(void)
+{
+	list_t node1, node2, node3;
+
+	node1.str = "Hello";
+	node1.len = 5;
+	node1.next = &node2;
+
+	node2.str = "World";
+	node2.len = 5;
+	node2.next = &node3;
+
+	node3.str = NULL;
+	node3.len = 0;
+	node3.next = NULL;
+
+	print_list(&node1);
+
+	return (0);
+}
