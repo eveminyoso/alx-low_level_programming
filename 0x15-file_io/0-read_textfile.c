4f77be9ad549d9d746b9358d71fbcf6889d9200c
eveminyoso <eveminyoso@gmail.com>
2023-09-04 13:42:03 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ *read_textfile - function that reads a text file and prints it
+ *@filename: file
+ *@: number to be printed
+ *Return: amount of bites
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t br, bw, tbr = 0;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	buffer = (char *)malloc(letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	while ((br = read(fd, buffer, letters)) > 0)
+	{
+		bw = write(STDOUT_FILENO, buffer, br);
+		if (bw == -1 || bw != br)
+		{
+			close(fd);
+			free(buffer);
+			return (0);
+		}
+		tbr += bw;
+	}
+	close(fd);
+	free(buffer);
+	return (tbr);
+}
