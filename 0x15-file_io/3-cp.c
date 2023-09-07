cbedbf5ff75c080cdcd52cfcb86f87f44e693543
eveminyoso <eveminyoso@gmail.com>
2023-09-07 13:01:28 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,73 +1,52 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
- *error_exit - error output
- *@code: codechecker
- *@format: format
- *@arg: arguement
- */
-void error_exit(int code, const char *format, const char *arg)
-{
-	dprintf(STDERR_FILENO, format, arg);
-	exit(code);
-}
-/**
- *main - copies and prints
- *@argc: arguement count
- *@argv:arguement array
- *Return:0
- */
+* main -copies the content of a file to another file
+* @argc: number of  argument
+* @argv: string argument
+* Return: 0
+*/
+
 int main(int argc, char *argv[])
 {
-	const char *file_from = argv[1];
-	const char *file_to = argv[2];
-	int fd_from, fd_to;
-	char buffer[1024];
-	ssize_t bytes_read, bytes_written;
+int file_from, file_to;
+int num1 = 1024, num2 = 0;
+char buf[1024];
 
-	if (argc != 3)
+if (argc != 3)
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+file_from = open(argv[1], O_RDONLY);
+if (file_from == -1)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
+}
+file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+	| S_IRGRP | S_IWGRP | S_IROTH);
+if (file_to == -1)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	close(file_from), exit(99);
+}
+while (num1 == 1024)
+{
+	num1 = read(file_from, buf, 1024);
+	if (num1 == -1)
 	{
-		error_exit(97, "Usage: cp file_from file_to\n", NULL);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
-	{
-		error_exit(98, "Error: Can't read from file %s\n", file_from);
-	}
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd_to == -1)
-	{
-		close(fd_from);
-		error_exit(99, "Error: Can't write to file %s\n", file_to);
-	}
-	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
+	num2 = write(file_to, buf, num1);
+	if (num2 < num1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+}
 
-		if (bytes_written == -1)
-		{
-			close(fd_from);
-			close(fd_to);
-			error_exit(99, "Error: Can't write to file %s\n", file_to);
-		}
+if (close(file_from) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 
-		if (bytes_written != bytes_read)
-		{
-			close(fd_from);
-			close(fd_to);
-			error_exit(99, "Error: Write incomplete\n", NULL);
-		}
-	}
+if (close(file_to) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 
-	if (bytes_read == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		error_exit(98, "Error: Can't read from file %s\n", file_from);
-	}
-	if (close(fd_from) == -1 || close(fd_to) == -1)
-	{
-		error_exit(100, "Error: Can't close fd\n", NULL);
-	}
-
-	return (0);
+return (0);
 }
