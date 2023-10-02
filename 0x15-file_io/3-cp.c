4979fb73532a5be925e8c0f0aaf00537d34594f4
eveminyoso <eveminyoso@gmail.com>
2023-10-02 12:30:20 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,46 +1,62 @@
 #include "main.h"
 /**
- *copyFile - copying function
- *@file_from: source file
- *@file_to: destination file
+ *main - copies content of a file to another
+ *@argc: arguement count
+ *@argv: arguement array
+ *Return: 0 value
  */
-void copyFile(const char *file_from, const char *file_to)
+int main(int argc, char *argv[])
 {
+	const char *file_from = argv[1], *file_to = argv[2];
 	int fd_from, fd_to;
-	char buffer[BUFFER_SIZE];
+	char buffer[1024];
 	ssize_t bytes_read, bytes_written;
-	/*Open the source file for reading*/
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
-		handleError("Error: Can't read from file %s\n", 98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
 	}
-	/*Create or open the destination file for writing, with appropriate permissions*/
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		handleError("Error: Can't write to file %s\n", 99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
 	}
-	/*Copy the contents from file_from to file_to*/
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1)
 		{
-			handleError("Error: Can't write to file %s\n", 99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			exit(99);
 		}
 	}
 	if (bytes_read == -1)
 	{
-		handleError("Error: Can't read from file %s\n", 98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
 	}
-	/*Close the file descriptors*/
 	if (close(fd_from) == -1)
 	{
-		handleError("Error: Can't close fd %d\n", 100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
 	}
 	if (close(fd_to) == -1)
 	{
-		handleError("Error: Can't close fd %d\n", 100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
 	}
+	if (chmod(file_to, 0664) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't set permissions for file %s\n", file_to);
+		exit(95);
+	}
+	return (0);
 }
