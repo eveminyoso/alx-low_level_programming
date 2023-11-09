6b10aed4962e529403703e1298bce9eef49f3134
eveminyoso <eveminyoso@gmail.com>
2023-11-09 13:17:15 -0800

    6-sum_dlistint.c

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - Returns the sum of all data (n) in a doubly linked list
+ * @head: Pointer to the head of the list
+ * Return: The sum of all data values or 0 if the list is empty
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
