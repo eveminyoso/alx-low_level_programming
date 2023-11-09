b848fbe2ecf71b20e2718d30155b58d63900e5cb
eveminyoso <eveminyoso@gmail.com>
2023-11-09 12:09:29 -0800

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,9 +12,10 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t node_count = 0;
 
 	while (h != NULL)
+	{
 		printf("%d\n", h->n);
-			node_count++;
+		node_count++;
+	}
 	h = h->next;
-}
 return (node_count);
 }
