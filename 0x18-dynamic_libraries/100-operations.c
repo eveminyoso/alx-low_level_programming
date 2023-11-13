c044fe29c2d2d5dc996c07b5258beebad2d4777a
eveminyoso <eveminyoso@gmail.com>
2023-11-13 12:31:28 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ *add - adds a and b
+ *@a:first number
+ *@b:second number
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *sub - subtracts a and b
+ *@a:first number
+ *@b:second number
+ *Return:a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *mul - multiplies a and b
+ *@a:first number
+ *@b:second number
+ *Return:a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *div - divides a and b
+ *@a:first number
+ *@b:second number
+ *Return:a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *mod - modulus a and b
+ *@a:first number
+ *@b:second number
+ *Return:a % b
+ *
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
